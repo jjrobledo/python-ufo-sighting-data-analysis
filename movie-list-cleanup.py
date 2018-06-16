b8f38fac4fdd1f1b8880f77ee64fa5cd796a1500
Jose Robledo <jj.robledo@me.com>
2018-06-16 15:08:47 -0400

    added a movie lookup function to dataframe_cleaner collected a list of ufo movies from wikipedia

--- movie-list-cleanup.py
+++ movie-list-cleanup.py
@@ -1,14 +1,41 @@
 import pandas as pd
+from imdb import IMDb
+
 pd.set_option('display.expand_frame_repr', False)
 
 # Read in the CSV and assign it to the variable ufoDf
-file = pd.read_csv('ufo movies.csv')
-movieDf = pd.DataFrame(file)
+filename = pd.read_csv('ufo movies.csv')
+movieDf = pd.DataFrame(filename)
+ia = IMDb()
+
 
 
 df2 = movieDf['Movie'].str[:].str.split(' - ', expand=True) # Split the single column at the - and expand the resulting split strings into their own df column
 df2[1] = df2[1].str[:4]  # Strip everything but the first four characters assuming that is the release year and discard everything else.
 df2 = df2.dropna(how='all') # drop rows that contain NaN
 df2.columns = ['Movie', 'Year', 'Release Date'] # rename the columns
+#df2.to_csv('clean-movie-list.csv')
 
-df2.to_csv('clean-movie-list.csv')
+movieList = []
+for index, row in df2.iterrows():
+    i = str(row.Movie) + ' (' + str(row.Year) + ')'
+    movieList.append(i)
+
+
+def movieLookup():
+
+    df3 = pd.DataFrame(columns=['Movie', 'Release Date'])
+    count = 0
+    for movie in movieList:
+
+        try:
+            movie_search = ia.search_movie(movie)
+            movie_id = movie_search[0].movieID
+            movie = ia.get_movie(movie_id)
+            df3 = df3.append({'Movie': str(movie), 'Release Date': str(movie['original air date'])}, ignore_index=True)
+            count += 1
+            print(count)
+        except (KeyError, IndexError):
+            pass
+
+    df3.to_csv('ufo-movie-releases')
