6b34927ca658a9571a188f4093095b150abc1950
Jose Robledo <jj.robledo@me.com>
2018-06-23 17:07:33 -0400

    Many changes made

--- ufo_data_analysis.py
+++ ufo_data_analysis.py
@@ -2,42 +2,41 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-import calendar
+from datetime import timedelta
+import datetime
+
 # TODO Combine shapeSightingsYear and shapeSightingsMonth by adding shapes to shapeSightingsMonth.
 # TODO move lines 14-16 to dataframe_cleaner.py
 
 pd.set_option('display.expand_frame_repr', False)
 
 ufoDf = pd.read_csv('ufo_reports.csv')
+ufoDf.Date = pd.to_datetime(ufoDf[['Year', 'Month', 'Day']], errors='coerce')
 
 #ufoDf['Month'] = ufoDf['Month'].apply(lambda x: calendar.month_name[x])
-ufoDf['Shape'] = ufoDf['Shape'].fillna(value='EMPTY')
-ufoDf['Shape'] = ufoDf['Shape'].str.upper()
 
-ufoShapeArray = []
-for i in ufoDf.Shape.str.upper().unique():
-    ufoShapeArray.append(i)
 
-def countShapes():
+def shapeGraph():
+    """
+    Generates a bar graph with a list of shapes on the y-axis and their frequency of the x-axis.
+
+    :return: graph
+    """
+    ufoShapeArray = []
+    for i in ufoDf.Shape.str.upper().unique():
+        ufoShapeArray.append(i)
+
     shapeCounts = pd.DataFrame(index=[ufoShapeArray], columns=['Count'])
     for i in ufoShapeArray:
         shapeCounts.loc[i] = ufoDf.Shape.str.contains(i).sum()
-    shapeCounts = shapeCounts.drop(['EMPTY', 'TRIANGULAR', 'HEXAGON', 'FLARE', 'CRESCENT', 'DELTA', 'PYRAMID', 'ROUND', 'DOME', 'CHANGED'])
-    return shapeCounts
+    shapeCounts = shapeCounts.drop(['EMPTY', 'HEXAGON', 'CRESCENT','PYRAMID', 'DOME']) # Remove useless values
+    shapeCounts = shapeCounts.reset_index()
+    shapeCounts.columns = ['Shape', 'Count']
+    shapes = shapeCounts.Shape
 
-def shapeGraph(dataframe):
-    # TODO fix the formatting of the names.
-    """
-    Takes countShapes() as the input dataframe.
-    Generates a bar graph with a list of shapes on the y-axis and their frequency of the x-axis.
-
-    :param dataframe:
-    :return:
-    """
     fig, ax = plt.subplots()
-    shapes = dataframe.index.values
-    ypos = np.arange(len(dataframe.index.values))
-    num_reports = dataframe.Count.values
+    ypos = np.arange(len(shapeCounts.index.values))
+    num_reports = shapeCounts.Count.values
 
     ax.barh(ypos, num_reports, align='center', color='green')
     ax.set_yticks(ypos)
@@ -46,30 +45,27 @@ def shapeGraph(dataframe):
 
     plt.show()
 
-
-def years():
+def sightigsByYear():
     """
-    The national UFO reporting center opened in 1974. I assume that only the data from 1974
-    will be comprehensive enough to be useful. Only collect the years starting then.
+    Plot the frequency of UFO sightings by year
 
-    :return:
+    :return: line plot of ufo sightings by year
     """
     yearDf = ufoDf.Year.value_counts()
     yearDf = yearDf.sort_index(ascending=False)
     yearDf = yearDf.iloc[:-75]
     yearDf = yearDf.reset_index()
     yearDf.columns = ['Year', 'Number of Sightings']
-    return yearDf
 
-def sightigsByYearGraph(dataframe):
-    """
-    Plot the frequency of UFO sightings by shape
+    plt_x = yearDf.Year
+    plt_y = yearDf['Number of Sightings']
 
-    :param dataframe: years()
-    :return:
-    """
-    plt_x = dataframe.Year
-    plt_y = dataframe['Number of Sightings']
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_title('Number of Sightings by Year')
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Number of Sightings')
+
     plt.plot(plt_x, plt_y)
     plt.show()
 
@@ -92,7 +88,6 @@ def sightingsByShape():
 
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-
     for shape in df.Shape.unique():
         xAxis = df1['Number of Occurances'][:, shape][-45:].index
         yAxis = df1['Number of Occurances'][:, shape][-45:].values
@@ -104,15 +99,13 @@ def sightingsByShape():
     ax.set_ylabel('Number of Sightings')
     plt.show()
 
-    # months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
 
 def annualHeatmap():
     # TODO change the names on the y axis to month names
     """
+    plots a heatmap using seaborn
 
-
-    :return:
+    :return: plot
     """
     df2 = ufoDf.groupby(['Year', 'Month', 'Date', 'Shape']).agg(len)
     df2 = df2.drop(['Duration', 'Summary', 'Time', 'State',
@@ -132,12 +125,116 @@ def annualHeatmap():
                      cbar_kws={"orientation": "horizontal"})
     plt.show()
 
-def movieSightings(year, month, daterange):
+
+movieDf2 = pd.read_csv('ufo-movie-releases.csv')
+movieDf2 = movieDf2.drop(['Unnamed: 0', 'Unnamed: 0.1.1'], axis=1)
+movieDf2["Release Date"] = pd.to_datetime(movieDf2['Release Date']) # Change the dtpye of the release date col to type datetime
+
+def getDateRange(movieReleaseDate):
+    '''
+    This function should probably go above the movieSightings function
+    This function will take a datetime object and return a range of days - 10 before and after the datetime entered.
+
+
+    :param movieReleaseDate:
+    :return: list of datetime objects
+    '''
+    day_list = []
+    dateHigh = movieReleaseDate + timedelta(days=8)
+    dateLow = movieReleaseDate - timedelta(days=7)
+    delta = dateHigh - dateLow
+    for i in range(delta.days):
+        day_list.append(dateLow + timedelta(i))
+
+
+
+
+    return day_list
+
+def movieSightings(indexvalue):
+    '''
+    This function will find the number of dates for a given day
+    :param date: datetime object
+    :return: the number of sightings for a give day
+
+    '''
     df2 = ufoDf.groupby(['Year', 'Month', 'Date', 'Shape']).agg(len)
     df2 = df2.drop(['Duration', 'Summary', 'Time', 'State',
                                                     'City', 'Posted'], axis=1)
     df2.columns = ['Number of Occurances']
     df2 = df2.unstack(fill_value=0)
     df2 = df2.stack()
+    list = []
+    date = movieDf2.iloc[indexvalue]['Release Date']
 
-    return df2.loc[year]['Number of Occurances'][month].loc[daterange].sum()
+    for i in getDateRange(date):
+
+        try:
+            list.append(df2.loc[i.year].loc[i.month]['Number of Occurances'].loc[i.day].sum())
+        except KeyError:
+            list.append(0)
+
+    npList = np.array(list)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    xaxis = np.arange(-7, 8, 1)
+    ax.set_xlabel('Days Before and After Release (0 = Release Day)')
+    ax.set_ylabel('Number of Sightings')
+    ax.set_title('Number of UFO Sightings During the Release of ' + str(movieDf2.iloc[indexvalue]['Movie']))
+
+    plt.xticks(np.arange(-7, 8, 1))
+    plt.plot(xaxis, npList)
+
+    print('The average number of daily sightings for the release window of ' + str(movieDf2.iloc[indexvalue]['Movie']) + ' was ' + str(npList.mean()) + ' sightings per day')
+    print('The average number of daily sightings for the year was ' + str(df2.loc[date.year].values.mean()))
+    plt.show()
+
+    plt.plot(df2.loc[date.year])
+    plt.show()
+    df2.loc[movieDf2.iloc[indexvalue]['Release Date'].year].unstack(fill_value=0).stack().groupby(['Month', 'Date']).sum().groupby(['Month', 'Date']).mean()
+    df2.loc[movieDf2.iloc[indexvalue]['Release Date'].year].unstack(fill_value=0).stack().groupby(
+        ['Month', 'Date']).sum()['Number of Occurances'].unstack(fill_value=0).stack()[1]
+
+
+def yearGraph():
+    mask = (ufoDf['Date'] > '1987-1-1') & (ufoDf['Date'] <= '1987-12-31')
+    time_series = pd.DataFrame(
+        ufoDf.loc[mask]['Date'].value_counts())  # it may be worthwhile to reset the index and sort the col
+    time_series = time_series.resample('D').sum().fillna(0)
+    time_series = time_series.sort_index()
+    time_series = time_series.reindex(pd.date_range("1987-01-01", "1987-12-31"), fill_value=0)
+    time_series = time_series.reset_index()
+
+
+    plt.plot(time_series['index'], time_series['Date'])
+    plt.show()
+
+def yearGraphSeasonal():
+    mask = (ufoDf['Date'] > '1987-1-1') & (ufoDf['Date'] <= '1987-12-31')
+    time_series = pd.DataFrame(
+        ufoDf.loc[mask]['Date'].value_counts())  # it may be worthwhile to reset the index and sort the col
+    time_series = time_series.resample('D').sum().fillna(0)
+    time_series = time_series.sort_index()
+    time_series = time_series.reindex(pd.date_range("1987-01-01", "1987-12-31"), fill_value=0)
+    time_series = time_series.reset_index()
+
+    data = time_series['Date'].rolling(12).mean()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    plt.plot(time_series['index'], data)
+    nyt_data = [(datetime(1987, 3, 1), 'Communion enters NYT Bestsell List at #12'),
+                (datetime(1987, 3, 25), 'Communion reaches #3'),
+                (datetime(1987, 5, 10), 'Communion is the #1 Bestseller'),
+                (datetime(1987, 7, 2), 'Communion falls out of top 3'),
+                (datetime(1987, 8, 9), 'Falls to #11'),
+                (datetime(1987, 8, 30), 'Rises to #8'),
+                (datetime(1987, 9, 20), 'Last week on list as #15')]
+    for date, label in nyt_data:
+        ax.annotate(label,xy=data.asof(date) + 75),
+                    xytext=(date, data.asof(date) + 225),
+                    arrowprops=dict(facecolor='red', headwidth=4, width=2, headlenght=4),
+                    horizontalalingment='left', verticalalignment='top')
+
+    plt.show()    data = time_series['Date'].rolling(12).mean()
